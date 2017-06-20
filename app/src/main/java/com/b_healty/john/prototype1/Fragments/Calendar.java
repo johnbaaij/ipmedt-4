5d68fd45c0d12ab5f41c972768f99f147f5f5c41
Ben van Eekelen <benvaneekelen674@gmail.com>
2017-06-20 11:17:50 +0200

    create progressbar from individual assets

--- app/src/main/java/com/b_healty/john/prototype1/Fragments/Calendar.java
+++ app/src/main/java/com/b_healty/john/prototype1/Fragments/Calendar.java
@@ -12,7 +12,8 @@ import com.b_healty.john.prototype1.R;
  * Created by John on 06/06/2017.
  */
 
-public class Calendar extends Fragment {
+public class Calendar extends
+        Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,7 +21,6 @@ public class Calendar extends Fragment {
 
         View view = inflater.inflate(R.layout.calendar_layout, container, false);
 
-        // TODO: 18-6-2017 Het toetsenbord duwt de hele layout omhoog
         // TODO: 18-6-2017 Er moeten nog animaties gemaakt worden voor de progressbar 
         // TODO: 18-6-2017 Voor de tijd input en de datum input gestroomlijnde manieren 
         // TODO: 18-6-2017 Alle front end stuff moet gekoppeld worden aan een back-end 
